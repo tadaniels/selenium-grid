8181aa1e119189d017a6b3e83d6756d4221191ce
ph7 <ph7@22590ab4-ef3b-0410-9bef-a9f5a1089f47>
2009-05-15 19:44:40 +0000

    Fixing the Ruby examples
    git-svn-id: https://svn.openqa.org/svn/selenium-grid/trunk@534 22590ab4-ef3b-0410-9bef-a9f5a1089f47

--- examples/ruby/flickr_example.rb
+++ examples/ruby/flickr_example.rb
@@ -10,12 +10,6 @@ module FlickrExample
     page.click "media_photos"
     page.click "//input[@value='SEARCH']", :wait_for => :page
     page.text?(options[:search_string].split(/ /).first).should be_true
-    page.click "//img[@class='pc_img']", :wait_for => :page
-    if page.text? "photo_gne_button_zoom"
-      page.click "photo_gne_button_zoom"
-    end
-    page.go_back :wait_for => :page
-    page.click "//img[@class='pc_img']", :wait_for => :page
   end
   
 end
