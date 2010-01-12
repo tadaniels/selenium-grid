986b61a0b740998ebe3fb43ba1da1176baa048ee
Philippe Hanrigou <philippe.hanrigou@gmail.com>
2010-01-11 22:28:17 -0800

    Upgrading Selenium RC and fixing demo+examples

--- demo/src/main/java/com/thoughtworks/selenium/grid/demo/GoogleImageTestBase.java
+++ demo/src/main/java/com/thoughtworks/selenium/grid/demo/GoogleImageTestBase.java
@@ -18,11 +18,9 @@ public abstract class GoogleImageTestBase {
         session().type("q", searchString);
         session().click("btnG");
         session().waitForPageToLoad(TIMEOUT);
-        session().click("link=Advanced Image Search");
-        session().waitForPageToLoad(TIMEOUT);
-        session().click("rimgtype4");
-        session().click("sf");
-        session().select("imgc", "full color");
+        session().click("rptgl");
+        session().click("imgsz_l");
+        session().click("imgtype_photo");
         session().click("btnG");
         session().waitForPageToLoad(TIMEOUT);
     }
--- examples/ruby/perigord_spec.rb
+++ examples/ruby/perigord_spec.rb
@@ -1,22 +1,22 @@
 require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
 
 describe "Perigord" do
-  include FlickrExample
+  include GoogleImageExample
   
   it "Has magnific cro-magnon paintings" do
-    run_flickr_scenario :search_string => "lascaux hall of the bull"
+    run_scenario :search_string => "lascaux hall of the bull"
   end
 
   it "Has magnific cities" do
-    run_flickr_scenario :search_string => "Sarlat"
+    run_scenario :search_string => "Sarlat"
   end
 
   it "Has magnific cathedrals" do
-    run_flickr_scenario :search_string => "Cathedral in Périgueux"
+    run_scenario :search_string => "Cathedral in Périgueux"
   end
 
   it "Has great wines" do
-    run_flickr_scenario :search_string => "Montbazillac"
+    run_scenario :search_string => "Montbazillac"
   end
 
 end
